c291a17d97ac598327d73813f0f10dddd4c8dd00
邵一鸣 <shaoym02@vanke.com>
2024-02-01 13:57:15 +0800

    fix

--- test/plugins/scenarios/go-elasticsearchv7/main.go
+++ test/plugins/scenarios/go-elasticsearchv7/main.go
@@ -1,8 +1,14 @@
 package go_elasticsearchv7
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	_ "github.com/apache/skywalking-go"
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
+	"log"
+	"net/http"
 )
 
 var testCases []struct {
@@ -10,21 +16,88 @@ var testCases []struct {
 	fn       testFunc
 }
 
-type testFunc func(ctx context.Context) error
+type testFunc func(es *elasticsearch.Client) error
 
 func init() {
+	log.Println("-----start append testCases------")
 	append(testCases, struct {
 		caseName string
 		fn       testFunc
 	}{caseName: "testIndex", fn: testIndex})
+	log.Println("-----finish append testCases------")
 }
 
-func main() {
-	for i := range testCases {
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
 
+func executeHandler(w http.ResponseWriter, r *http.Request) {
+	es := initClient()
+	for _, testCase := range testCases {
+		log.Printf("start execute test case: %s", testCase.caseName)
+		err := testCase.fn(es)
+		if err != nil {
+			log.Fatalf("fail to execute test case,name:%s", testCase.caseName)
+		}
+		log.Printf("finish execute test case: %s", testCase.caseName)
 	}
 }
 
-func testIndex(ctx context.Context) error {
+func initClient() *elasticsearch.Client {
+	address := []string{"http://43.139.166.178:9200"}
 
+	cfg := elasticsearch.Config{
+		Addresses: address,
+	}
+	es, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	info, err := es.Info()
+	defer info.Body.Close()
+	log.Println("connect es success")
+	return es
+}
+
+func main() {
+
+	http.HandleFunc("/health", healthHandler)
+
+	http.HandleFunc("/execute", executeHandler)
+
+	err := http.ListenAndServe(":8000", nil)
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+func testIndex(es *elasticsearch.Client) error {
+
+	data, err := json.Marshal(struct {
+		name string
+	}{
+		name: "sw-go",
+	})
+	if err != nil {
+		log.Fatalf("Error marshaling document: %s", err)
+	}
+
+	log.Println(string(data))
+
+	req := esapi.IndexRequest{
+		Index:      "sw-index",
+		Body:       bytes.NewReader(data),
+		DocumentID: "1",
+		Refresh:    "true",
+	}
+
+	res, err := req.Do(context.Background(), es)
+
+	if err != nil || res.IsError() {
+		log.Fatalf("Error getting index request'response: %s", err)
+	}
+
+	defer res.Body.Close()
+	return nil
 }
--- test/plugins/scenarios/zap/go.mod
+++ test/plugins/scenarios/zap/go.mod
@@ -1,3 +1,3 @@
 module test/plugins/scenarios/zap
 
-go 1.18
+go 1.19
